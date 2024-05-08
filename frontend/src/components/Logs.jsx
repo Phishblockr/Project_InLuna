7e51bbb22771cced91f2ef8d2bcaaefc0d2dbcde
Aditya <adityapaluskar@gmail.com>
2024-05-08 18:32:00 +0530

    Modified Logs page

--- frontend/src/components/Logs.jsx
+++ frontend/src/components/Logs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Logs() {
   return (
-    <div className='flex absolute top-[63px] left-[16rem] justify-center items-center right-0 bottom-0'>
+    <div>
       <h1>Logs Page</h1>
     </div>
   )
