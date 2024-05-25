a8bdc7e854a5569026abca2260037bf428321e5a
Aditya <adityapaluskar@gmail.com>
2024-05-25 17:17:41 +0530

    Initiated node backend

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
+app.listen(5000, () => console.log('running on 5000'));
