97f775489ecdd0293c22aadee5f3b2a36245517d
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2024-05-31 00:25:52 +0530

    fix dropdown menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   RiSearchLine,
   RiNotificationBadgeLine,
@@ -8,7 +8,7 @@ import { FaRegUserCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const profileBtnStyle =
-  "bg-[#0364BD] py-2 rounded-lg text-white transition hover:bg-[#003A70]";
+  "bg-[#0364BD] rounded-lg text-white transition hover:bg-[#003A70]";
 
 const profileSettings = [
   {
@@ -27,11 +27,21 @@ const profileSettings = [
     id: 3,
     title: "Log Out",
     url: "#",
-    style: "bg-red-500 py-2 rounded-lg text-white transition hover:bg-red-700",
+    style: "bg-red-500 rounded-lg text-white transition hover:bg-red-700",
   },
 ];
 
 const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdown(!dropdown);
+  };
+
+  const closeDropdown = () => {
+    setDropdown(false);
+  };
+
   return (
     <nav className="bg-white px-4 py-3 flex justify-between sticky left-0 right-0 top-0 ml-64 z-10">
       <div className="flex items-center text-x1">
@@ -58,18 +68,32 @@ const Navbar = () => {
             <span>User</span>
             <span className="text-[10px]">Admin</span>
           </div>
-          <button className="text-black group">
+          <button onClick={toggleDropdown} className="text-black group">
             <RiArrowDropDownLine className="w-6 h-6 mt-1" />
-            <div className="z-10 hidden absolute bg-white rounded-lg shadow w-32 group-focus:block top-full right-0">
-              <ul className="p-2 text-sm text-gray-950 gap-1 flex flex-col">
-                {profileSettings.map((setting) => (
-                  <li key={setting.id} className={setting.style}>
-                    <Link to={setting.url}>{setting.title}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
           </button>
+          {dropdown && (
+            <>
+              <div
+                className="fixed inset-0 z-10"
+                onClick={closeDropdown}
+              ></div>
+              <div className="z-20 absolute bg-white rounded-lg shadow w-32 top-full right-0">
+                <ul className="p-2 text-sm text-gray-950 gap-1 flex flex-col">
+                  {profileSettings.map((setting) => (
+                    <li key={setting.id} className={setting.style}>
+                      <Link
+                        onClick={closeDropdown}
+                        className="flex items-center justify-center py-2"
+                        to={setting.url}
+                      >
+                        {setting.title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </nav>
