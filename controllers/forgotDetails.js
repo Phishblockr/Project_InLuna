53b6d9bb52204c51222ab48fd43c4f69213aa8d2
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2024-11-15 01:04:48 +0530

    update email-template

--- controllers/forgotDetails.js
+++ controllers/forgotDetails.js
@@ -50,12 +50,52 @@ const generatePasswordResetLink = async (user, userId, orgId) => {
     return `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`;
 };
 
-const sendEmail = async (to, subject, content) => {
+const sendEmail = async (to, subject, content, user) => {
     const msg = {
         to,
         from: process.env.VERIFIED_SENDER_EMAIL,
         subject,
-        html: `${content}<br/><p>Thank you,</p><p>Team Phishblockr</p>`,
+        html: `
+        <body>
+    <div style="font-family: sans-serif; max-width: 600px; margin: 0 auto;  font-size: 18px; color: #333; background-color: #f4f4f4;">
+    <header style="padding: 26px; background-color: #0364BD; color: #f4f4f4; font-size: 24px; display: flex; align-items: center; gap: 26px; border-radius: 0px 0px 10px 10px; box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="47.5 47.5 105 105">
+            <circle cx="100" cy="100" r="50" fill="white" stroke="red" stroke-width="5"></circle>
+            <circle cx="100" cy="100" r="25" fill="black"></circle>
+        </svg>
+        <span style="font-weight: bold;">Phishblockr - Support</span>
+    </header>
+    <div style="padding: 10px; width: 100%;">
+        <p>Hi ${user.name}</p>
+        <p>We received your request for account assistance. Here are the details:</p>
+        <span>${content}</span>
+        <p>If you did not make this request, it's possible someone else is trying to access your Phishblockr account. <br> <strong>Please ignore this email if you did not request assistance.</strong></p>
+        <p>Sincerely yours,</p>
+        <p>The Phishblockr team</p>
+    </div>
+    <footer style="padding: 20px; font-size: 14px; color: #777; text-align: center; background-color: #0364BD; color: #f4f4f4; border-radius: 10px 10px 0px 0px;">
+        <p>If you need further assistance, please contact our support team at 
+            <a href="mailto:support@excellitude.com" style="color: #f4f4f4; text-decoration: none;">support@phishblockr.com</a>.
+        </p>
+        <p style="margin-top: 10px;">Excellitude Pvt ltd. | 1234 Cybersecurity Lane, Suite 100 | Security City, SC 12345</p>
+        <p style="margin-top: 10px;">
+            <a href="https://phishblockr.com/privacy-policy" style="color: #f4f4f4; text-decoration: none;">Privacy Policy</a> | 
+            <a href="https://phishblockr.com/terms-of-service" style="color: #f4f4f4; text-decoration: none;">Terms of Service</a>
+        </p>
+    </footer>
+    </div>
+    
+    <style>
+        /* Ensures mobile style on both desktop and mobile */
+        @media screen and (max-width: 600px) {
+            div[style*="max-width: 600px;"] {
+                padding: 20px;
+                font-size: 16px;
+                text-align: center;
+            }
+        }
+    </style>
+        `,
     };
     await sgMail.send(msg);
 };
@@ -98,7 +138,7 @@ export const handleForgotDetails = async (req, res) => {
         }
 
         // Send the combined email
-        await sendEmail(email, "Phishblockr Dashboard - Forgot Details Assistance", emailContent);
+        await sendEmail(email, "Phishblockr Dashboard - Forgot Details Assistance", emailContent, user);
 
         res.status(200).json({ message: responseMessage.join(" ") });
     } catch (error) {
