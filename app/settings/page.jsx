36a3d3eedbc0085c8eea69aac31344be32bf4fee
Pratham02 <prathamshinde2137@gmail.com>
2025-01-09 15:30:58 +0530

    initial commit

--- app/settings/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div className='h-screen bg-white flex justify-center items-center'>
-      <h1>Settings Page</h1>
-    </div>
-  )
-}
