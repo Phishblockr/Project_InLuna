e04e24d24afea427d059f4224d7dcae32b506f7c
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2025-01-16 12:06:22 +0530

    add pagination and filter for email templates

--- routes/trainingPlatform/emailTemplateRoutes.js
+++ routes/trainingPlatform/emailTemplateRoutes.js
@@ -1,16 +1,18 @@
 import express from "express";
 import { createEmailTemplate, deleteEmailTemplate, editEmailTemplate, getAllEmailTemplates, getEmailTemplateById, getTemplatesGroups } from "../../controllers/trainingPlatform/emailTemplateController.js";
+import superDashboardMiddleware from "../../middlewares/superDashboardMiddleware.js"
+import authenticateToken from "../../middlewares/authenticateToken.js";
 
 const router = express.Router();
 
 // Email template CRUD Operations
-router.post("/create", createEmailTemplate);
-router.get("/getAll", getAllEmailTemplates);
-router.get("/get/:id", getEmailTemplateById);
-router.delete("/delete/:id", deleteEmailTemplate);
-router.put("/update/:id", editEmailTemplate);
+router.post("/create", superDashboardMiddleware, createEmailTemplate);
+router.get("/getAll", authenticateToken, getAllEmailTemplates);
+router.get("/get/:id", authenticateToken, getEmailTemplateById);
+router.delete("/delete/:id", superDashboardMiddleware, deleteEmailTemplate);
+router.put("/update/:id", superDashboardMiddleware, editEmailTemplate);
 
 // Get groups for dynamic select
-router.get("/getGroups", getTemplatesGroups);
+router.get("/getGroups", authenticateToken, getTemplatesGroups);
 
 export default router;
