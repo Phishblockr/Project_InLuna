e28008ebb7eda2ac37723790e4a4e1151fbabd8d
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2025-02-22 01:20:32 +0530

    add advance email-template options

--- models/trainingPlatform/emailTemplateModel.js
+++ models/trainingPlatform/emailTemplateModel.js
@@ -6,17 +6,57 @@ const emailTemplateSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    subject: {
+        type: String,
+        require: true
+    },
+    isSubjectPhishing: {
+        type: Boolean,
+        default: false
+    },
+    senderAddress: {
+        type: String,
+        require: true
+    },
+    isSenderAddressPhishing: {
+        type: Boolean,
+        default: false
+    },
+    mailedBy: {
+        type: String,
+        require: true
+    },
+    isMailedByPhishing: {
+        type: Boolean,
+        default: false
+    },
+    signedBy: {
+        type: String,
+        required: true,
+    },
+    isSignedByPhishing: {
+        type: Boolean,
+        default: false
+    },
+    securityProtocol: {
+        type: String,
+        required: true,
+    },
+    isSecurityProtocolPhishing: {
+        type: Boolean,
+        default: false
+    },
     htmlContent: {
         type: String,
         required: true,
     },
-    group:{
-        type:String,
-        required:true,
+    group: {
+        type: String,
+        required: true,
     },
-    isPhishing:{
-        type:Boolean,
-        required:true
+    isPhishing: {
+        type: Boolean,
+        required: true
     },
 },
     { timestamps: true }
