26c5819cbe0693f86822bf4da7b89d5122b9a36f
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2025-02-25 15:13:07 +0530

    made changes for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ import SetupPassword from "./components/Users/SetupPassword.jsx";
 import Layout from "./layout/Layout.jsx";
 import Training from "./components/Training/Training.jsx";
 import IndividualTraining from "./components/Training/IndividualTraining.jsx";
-import RedirectToSubdomain from "./components/RedirectToSubdomain.jsx";
+// import RedirectToSubdomain from "./components/RedirectToSubdomain.jsx";
 
 function App() {
   const theme = useSelector((state) => state.theme);
@@ -51,7 +51,7 @@ function App() {
           <div className="w-full h-full">
             <Router>
               <AuthProvider>
-                <RedirectToSubdomain />
+                {/* <RedirectToSubdomain /> */}
                 <Routes>
                   <Route path="/login" element={<Login />} />
                   <Route path="/logout" element={<Logout />} />
