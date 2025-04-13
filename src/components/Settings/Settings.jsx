c4cbf61ccf257f41bb58f1547553c155009a2d5b
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2025-04-13 20:47:57 +0530

    fix navigation issue and add make payment button

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -2,56 +2,79 @@ import React from "react";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import { setTheme } from "../../features/Theme/themeSlice";
+import { useAuth } from "../../utils/AuthProvider";
 
 const Settings = () => {
-    // Redux
-    const theme = useSelector((state) => state.theme);
-    const dispatch = useDispatch();
-    // End of Redux
+  // Redux
+  const theme = useSelector((state) => state.theme);
+  const dispatch = useDispatch();
+  // End of Redux
 
-    const handleSetTheme = (value) => {
-        dispatch(setTheme(value))
-    }
+  const { getToken } = useAuth();
+  const token = getToken();
 
+  const handleSetTheme = (value) => {
+    dispatch(setTheme(value));
+  };
 
-    return (
-        <div className="z-1 h-[calc(100vh-65px)] flex flex-col justify-between relative right-0 bottom-0 p-4 gap-4">
-            <div className="z-1 relative w-full font-medium bg-white rounded-xl shadow-xl p-3 h-full dark:bg-[#002451]">
-                <div>
-                    <h1 className="mb-10 pt-3 pl-5 text-2xl font-medium dark:text-[#F4F4F4]">
-                        Phisblokr Settings
-                    </h1>
-                </div>
-                <div className="absolute top-20 bottom-5 left-5 right-5 flex flex-col justify-between">
-                    <div className="grid grid-cols-[minmax(10%,_1fr)_300px]  gap-5">
-                        <div className="p-2 bg-gray-100 rounded-lg dark:bg-[#001C40] dark:text-[#F4F4F4]">
-                            <span className="text-xl font-medium">Themes</span>
-                        </div>
-                        <div>
-                            <select
-                                className="w-full p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#0364BD] bg-gray-100 dark:bg-[#001c40] dark:text-[#F4F4F4]"
-                                name="theme"
-                                id="theme"
-                                onChange={(e) => handleSetTheme(e.target.value)}
-                                value={theme}
-                            >
-                                <option value="light">Light Theme</option>
-                                <option value="dark">Dark Theme</option>
-                            </select>
-                        </div>
-                        <div className="p-2 bg-gray-100 rounded-lg dark:bg-[#001C40] dark:text-[#F4F4F4]">
-                            <span className="text-xl font-medium">Manage Subscription</span>
-                        </div>
-                    </div>
-                    <div className="flex justify-end gap-2 dark:text-[#F4F4F4]">
-                        <a href="#" title="Privacy policy">
-                            <IoDocumentTextOutline className="w-6 h-6" />
-                        </a>
-                    </div>
-                </div>
-            </div>
+  const goToPaymentPage = async () => {
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const res = await fetch(`${apiUrl}/ccavenue/encryptPayload`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    window.location.href = data.url;
+  };
+
+  return (
+    <div className="z-1 h-[calc(100vh-65px)] flex flex-col justify-between relative right-0 bottom-0 p-4 gap-4">
+      <div className="z-1 relative w-full font-medium bg-white rounded-xl shadow-xl p-3 h-full dark:bg-[#002451]">
+        <div>
+          <h1 className="mb-10 pt-3 pl-5 text-2xl font-medium dark:text-[#F4F4F4]">
+            InLuna Settings
+          </h1>
         </div>
-    );
+        <div className="absolute top-20 bottom-5 left-5 right-5 flex flex-col justify-between">
+          <div className="grid grid-cols-[minmax(10%,_1fr)_300px]  gap-5">
+            <div className="p-2 bg-gray-100 rounded-lg dark:bg-[#001C40] dark:text-[#F4F4F4]">
+              <span className="text-xl font-medium">Themes</span>
+            </div>
+            <div>
+              <select
+                className="w-full p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#0364BD] bg-gray-100 dark:bg-[#001c40] dark:text-[#F4F4F4]"
+                name="theme"
+                id="theme"
+                onChange={(e) => handleSetTheme(e.target.value)}
+                value={theme}
+              >
+                <option value="light">Light Theme</option>
+                <option value="dark">Dark Theme</option>
+              </select>
+            </div>
+            <div className="p-2 bg-gray-100 rounded-lg dark:bg-[#001C40] dark:text-[#F4F4F4] flex flex-col">
+              <span className="text-xl font-medium">Subscription Details</span>
+              <button
+                className="bg-white p-2 hover:shadow-lg transition-all"
+                onClick={() => goToPaymentPage()}
+              >
+                Make Payment
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-end gap-2 dark:text-[#F4F4F4]">
+            <a href="#" title="Privacy policy">
+              <IoDocumentTextOutline className="w-6 h-6" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,44 +1,51 @@
-import React, { useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import Sidebar2 from '../components/Navigation/Sidebar2';
-import Navbar from '../components/Navigation/Navbar';
+import React, { useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Sidebar2 from "../components/Navigation/Sidebar2";
+import Navbar from "../components/Navigation/Navbar";
 
 const Layout = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    const isLoginPage = location.pathname === '/login';
-    const isLogoutPage = location.pathname === '/logout';
-    const isForgotDetailsPage = location.pathname === '/forgotDetails';
-    const isResetPasswordPage = location.pathname.startsWith('/resetPassword');
-    const isPasswordResetSuccessfulPage = location.pathname === "/passwordResetSuccessful"
-    const isPasswordSetSuccessfulPage = location.pathname === "/passwordSetSuccessful"
+  const isLoginPage = location.pathname === "/login";
+  const isLogoutPage = location.pathname === "/logout";
+  const isForgotDetailsPage = location.pathname === "/forgotDetails";
+  const isResetPasswordPage = location.pathname.startsWith("/resetPassword");
 
-    const showSidebarAndNavbar =
-        !isLoginPage && !isLogoutPage && !isResetPasswordPage && !isForgotDetailsPage &&isPasswordResetSuccessfulPage && isPasswordSetSuccessfulPage;
+  const showSidebarAndNavbar =
+    !isLoginPage &&
+    !isLogoutPage &&
+    !isResetPasswordPage &&
+    !isForgotDetailsPage &&
+    location.pathname !== "/passwordResetSuccessful" &&
+    location.pathname !== "/passwordSetSuccessful";
 
-    // Lift sidebar state here
-    const [expanded, setExpanded] = useState(true);
+  // Lift sidebar state here
+  const [expanded, setExpanded] = useState(true);
 
-    // Define dynamic widths/margins based on expanded state
-    const sidebarWidth = expanded ? 'w-64' : 'w-20';
-    const contentMargin = expanded ? 'ml-64' : 'ml-20';
+  // Define dynamic widths/margins based on expanded state
+  const sidebarWidth = expanded ? "w-64" : "w-20";
+  const contentMargin = expanded ? "ml-64" : "ml-20";
 
-    return (
-        <div className="min-h-screen dark:bg-[#001733]">
-            {/* Pass expanded state to Navbar */}
-            {showSidebarAndNavbar && <Navbar expanded={expanded} />}
-            <div className="flex">
-                {showSidebarAndNavbar && (
-                    <div className={`${sidebarWidth} fixed left-0 top-0 h-full z-10 transition-all`}>
-                        <Sidebar2 expanded={expanded} setExpanded={setExpanded} />
-                    </div>
-                )}
-                <div className={`flex-grow transition-all ${showSidebarAndNavbar ? contentMargin : ''}`}>
-                    <Outlet />
-                </div>
-            </div>
+  return (
+    <div className="min-h-screen dark:bg-[#001733]">
+      {/* Pass expanded state to Navbar */}
+      {showSidebarAndNavbar && <Navbar expanded={expanded} />}
+      <div className="flex">
+        {showSidebarAndNavbar && (
+          <div
+            className={`${sidebarWidth} fixed left-0 top-0 h-full z-10 transition-all`}
+          >
+            <Sidebar2 expanded={expanded} setExpanded={setExpanded} />
+          </div>
+        )}
+        <div
+          className={`flex-grow transition-all ${showSidebarAndNavbar ? contentMargin : ""}`}
+        >
+          <Outlet />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
