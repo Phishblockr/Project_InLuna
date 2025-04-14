c02e2e8da0212b51eaa545ea854bfa997da988c4
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2025-04-14 10:48:17 +0530

    add cors

--- server.js
+++ server.js
@@ -73,6 +73,7 @@ const allowedOrigins = [
   "moz-extension://",
   "http://localhost:5174",
   "http://localhost:5175",
+  "http://localhost:5177",
   "http://localhost:5000",
   "https://theinluna.com",
   "https://dashboard.theinluna.com",
