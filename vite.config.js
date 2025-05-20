d512c4ad916ff57aa1852bd2b5d3bddd0b99245d
Pranav Dalvi <pranav.dalvi0128@gmail.com>
2025-05-21 01:07:23 +0530

    fix build issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "esnext", //browsers can handle the latest ES features
+  },
   server: {
     host: true, // Listen on all addresses (0.0.0.0)
-    allowedHosts: ['.lvh.me'] // Allow any subdomain of lvh.me
-  }
-})
+    allowedHosts: [".lvh.me"], // Allow any subdomain of lvh.me
+  },
+});
